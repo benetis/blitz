ac223928196430142f3209c62fe34e9b6f6fd0cf
benetis <git@benetis.me>
2024-06-23 16:51:59 +0300

    slice: filter mutex for shared memory

--- slice/filter.go
+++ slice/filter.go
@@ -10,6 +10,7 @@ func Filter[T any](inputs []T, fn func(T) bool, maxWorkers ...int) []T {
 	}
 
 	var wg sync.WaitGroup
+	var mu sync.Mutex
 
 	filterFlags := make([]bool, len(inputs))
 	chunkSize := calcChunkSize(len(inputs), numWorkers)
@@ -17,7 +18,11 @@ func Filter[T any](inputs []T, fn func(T) bool, maxWorkers ...int) []T {
 	worker := func(start int, end int) {
 		defer wg.Done()
 		for i := start; i < end; i++ {
-			filterFlags[i] = fn(inputs[i])
+			if fn(inputs[i]) {
+				mu.Lock()
+				filterFlags[i] = true
+				mu.Unlock()
+			}
 		}
 	}
 
